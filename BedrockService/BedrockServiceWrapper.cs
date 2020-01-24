1ff247f8b50b5224ba35a2de7f50dbcef2cb3214
Brendon Raw <brendon.raw@gmail.com>
2020-01-24 05:16:25 +0000

    Initial File Load

---
+++ BedrockService/BedrockServiceWrapper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BedrockService
+{
+    public class BedrockServiceWrapper
+    {
+
+        Process process;
+        Thread outputThread;
+        Thread errorThread;
+        Thread inputThread;
+        static BackgroundWorker bedrockServer;
+        
+        public BedrockServiceWrapper()
+        {
+            bedrockServer = new BackgroundWorker
+            {
+                WorkerSupportsCancellation = true
+            };
+          
+
+
+        }
+       
+
+       
+        public void Stop()
+        {
+
+
+            process.StandardInput.WriteLine("stop");            
+            bedrockServer.CancelAsync();
+            
+        }
+
+       
+
+        public void Start()
+        {
+            
+            bedrockServer.DoWork += (s, e) =>
+            {
+                
+
+                    RunServer();
+               
+            };
+            
+            bedrockServer.RunWorkerAsync();
+           
+
+        }
+
+        public void RunServer()
+        {
+            const string fileName = @"C:\Users\brend_rxz2yiy\source\repos\BedrockService\BedrockServer\bedrock_server.exe";
+            
+            // Fires up a new process to run inside this one
+            process = Process.Start(new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = fileName
+                 
+                
+                
+            });
+            
+            // Depending on your application you may either prioritize the IO or the exact opposite
+            const ThreadPriority ioPriority = ThreadPriority.Highest;
+            outputThread = new Thread(outputReader) { Name = "ChildIO Output", Priority = ioPriority };
+            errorThread = new Thread(errorReader) { Name = "ChildIO Error", Priority = ioPriority };
+            inputThread = new Thread(inputReader) { Name = "ChildIO Input", Priority = ioPriority };
+
+            // Set as background threads (will automatically stop when application ends)
+            outputThread.IsBackground = errorThread.IsBackground
+                = inputThread.IsBackground = true;
+
+            // Start the IO threads
+            outputThread.Start(process);
+            errorThread.Start(process);
+            inputThread.Start(process);
+
+            process.WaitForExit();
+
+        }
+
+        /// <summary>
+        /// Continuously copies data from one stream to the other.
+        /// </summary>
+        /// <param name="instream">The input stream.</param>
+        /// <param name="outstream">The output stream.</param>
+        private static void passThrough(Stream instream, Stream outstream)
+        {
+            byte[] buffer = new byte[4096];
+            while (true)
+            {
+                int len;
+                while ((len = instream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                   
+                    outstream.Write(buffer, 0, len);
+                    outstream.Flush();
+                }
+            }
+        }
+
+        private static void outputReader(object p)
+        {
+            var process = (Process)p;
+            // Pass the standard output of the child to our standard output
+            passThrough(process.StandardOutput.BaseStream, Console.OpenStandardOutput());
+        }
+
+        private static void errorReader(object p)
+        {
+            var process = (Process)p;
+            // Pass the standard error of the child to our standard error
+            passThrough(process.StandardError.BaseStream, Console.OpenStandardError());
+        }
+
+        private static void inputReader(object p)
+        {
+            var process = (Process)p;
+            // Pass our standard input into the standard input of the child
+            passThrough(Console.OpenStandardInput(), process.StandardInput.BaseStream);
+        }
+    }
+}
---
+++ BedrockService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace BedrockService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var rc = HostFactory.Run(x =>                                   
+            {
+                x.Service<BedrockServiceWrapper>(s =>                                  
+                {
+                    s.ConstructUsing(name => new BedrockServiceWrapper());                
+                    s.WhenStarted(tc => tc.Start());                         
+                    s.WhenStopped(tc => tc.Stop()); 
+                    
+
+                });
+                x.RunAsNetworkService();                                       
+                
+                x.SetDescription("Windows Service Wrapper for Windows Bedrock Server");                   
+                x.SetDisplayName("BedrockService");                                  
+                x.SetServiceName("BedrockService");                                  
+            });                                                             
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
