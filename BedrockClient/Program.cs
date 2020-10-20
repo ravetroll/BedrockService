b5f50d7491fdd5695c1798fa205120eca4263cd2
Bruno <brunofvalli@gmail.com>
2020-10-19 20:56:41 -0500

    +Adding multiple port number support.

--- BedrockClient/Program.cs
+++ BedrockClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using BedrockService;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
@@ -16,13 +17,42 @@ namespace BedrockClient
             Console.Title = "Minecraft Bedrock Service Console";
             Console.WriteLine("Minecraft Bedrock Service Console");
 
-            int portNumber = 19134;
+            if (args.Length == 0)
+            {
+                // we are going to get thu ports from our settings 
+                List<int> portNumberList = new List<int>();
 
+                foreach (var portNumber in portNumberList)
+                {
+                    using (Process p = new Process())
+                    {
+                        ProcessStartInfo info = new ProcessStartInfo();
+                        info.FileName = "BedrockClient";
+                        info.Arguments = portNumber.ToString();
+                        info.RedirectStandardInput = true;
+                        info.UseShellExecute = false;
+
+                        p.StartInfo = info;
+                        p.Start();
+                    }
+                }
+            }
+            else
+            {
+                if (int.TryParse(args[0], out int portNumber))
+                {
+                    ClientInstance(portNumber);
+                }
+            }
+        }
+
+        private static void ClientInstance(int portNumber)
+        {
             ClientConnector.Connect(Console.WriteLine, portNumber);
 
             // start the connection with server to get output
             Thread outputThread = new Thread(new ParameterizedThreadStart(ClientConnector.OutputThread)) { Name = "ChildIO Output Console" };
-            
+
             outputThread.Start(new ThreadPayLoad(Console.WriteLine, portNumber));
 
             while (true)
